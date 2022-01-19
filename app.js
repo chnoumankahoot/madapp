542d1d27cdc86542a804769ba3e8c231e5d8fa35
NOUMAN AHSAN <fa18-bse-071@isbstudent.comsats.edu.pk>
2022-01-19 19:38:48 +0500

    new

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const assert = require('assert');
 var cookieParser = require('cookie-parser');
+const MongoClient = require('mongodb').MongoClient;
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var cors = require('cors');
@@ -11,32 +13,61 @@ const corsOptions = {
   credentials: true
 };
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var adminRouter = require('./app_server/routes/admin');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'jade');
 
-var url = "mongodb+srv://admin:admin@cluster0.rhzqz.mongodb.net/trader";
-mongoose.connect(url, (err) => {
-  if (err) throw err;
-});
+
 
 app.options('*', cors(corsOptions));
-app.use('/uploads', express.static('uploads'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
+app.use(express.static(path.join(__dirname, 'public')));
+const checkAddress=async (client,address)=>{
+  const result = await  client.findOne({bep20:address});
+  if(result){
+      return true
+  }
+  else{return false}
+}
+
+
+var client1 ;
+MongoClient.connect(
+'mongodb+srv://test:hello@cluster0.ufjup.mongodb.net/myFirstDatabase?authSource=admin&replicaSet=atlas-j9lkc8-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
+{ useNewUrlParser: true, useUnifiedTopology: true },
+ function(connectErr, client) {
+  assert.equal(null, connectErr);
+  const coll = client.db('list').collection('users');
+ client1=coll;
+});
+//connecting to the list database
+
+app.get("/", (req, res) =>{
+res.send("Nothing");
+});
+app.get("/api/:address",  async function (req, res) {
+ 
+if (req.params.address.length < 10) {
+  res.send("Invalid address");
+} else {
+    check = await checkAddress(client1,req.params.address)
+  if(check)
+  {
+      res.json({isPresent:true})
+    
+  }
+  else{
+      res.json({isPresent:false})
+  }
+  
+}
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
